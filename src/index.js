e01413a3099fa46688d294f4567a82181f05795b
Artur Pakhomau <al9966@mail.ru>
2020-02-23 23:31:06 +0300

    feat:add task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let array =[];
+    let temp = true; 
+    if(n<0)
+    {   temp = false;
+         let tempArray =String(n).split('');
+        tempArray.splice(0, 1);
+        array = tempArray.reverse();
+    }else{
+        array = String(n).split('').reverse();
+    }
+    return array.join('');
 }
